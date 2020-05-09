f85e3a989e769eebbb4177bb04f2b2d691010fad
Pooria Soltani <pooria.ms@gmail.com>
2020-05-09 20:42:01 +0430

    Added info on how many days has passed since api launch.

--- khayyam_api/about.py
+++ khayyam_api/about.py
@@ -1,11 +1,24 @@
+from khayyam import JalaliDate
+
 from khayyam_api._version import __version__
 from khayyam_api.main import app
+
+start = JalaliDate(1399, 2, 18)
+today = JalaliDate.today()
+
+live_since = (
+    start.isoformat()
+    + f" ({start.todate().isoformat()}): "
+    + f"{(today - start).days} days ago!"
+)
+
 
 about_khayyam_api = {
     "name": "khayyam_api",
     "version": __version__,
     "description": "A dead simple Jalali API",
     "repo": "https://github.com/pmsoltani/khayyam_api",
+    "live_since": live_since,
     "author": {
         "name": "Pooria Soltani",
         "contact": {"linkedin": "https://www.linkedin.com/in/pmsoltani"},
