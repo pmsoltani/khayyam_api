0e933f52d31acf2bcf87a35eb357a005207a75f0
Pooria Soltani <pooria.ms@gmail.com>
2020-05-15 11:57:26 +0430

    Modified the 'live_since' section of the 'about.py'.

--- khayyam_api/about.py
+++ khayyam_api/about.py
@@ -3,22 +3,15 @@ from khayyam import JalaliDate
 import khayyam_api._info as info
 from khayyam_api.main import app
 
-start = JalaliDate(1399, 2, 18)
-today = JalaliDate.today()
-
-live_since = (
-    start.isoformat()
-    + f" ({start.todate().isoformat()}): "
-    + f"{(today - start).days} days ago!"
-)
-
+launch_date_j = JalaliDate(1399, 2, 18)
+launch_date_g = launch_date_j.todate()
 
 about_khayyam_api = {
     "name": info.__project_name__,
     "version": info.__version__,
     "description": info.__description__,
     "repo": info.__repo__,
-    "live_since": live_since,
+    "live_since": f"{launch_date_j.isoformat()} ({launch_date_g.isoformat()})",
     "author": {
         "name": info.__author__,
         "contact": {"email": info.__email__, "linkedin": info.__linkedin__},
